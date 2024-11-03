44aa40d3c34e73adf4b25ee5ea5eb22fc5b8882e
kadw108 <wallwritinglast@gmail.com>
2024-11-02 20:44:04 -0400

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+
+    <h1>Eldritch Experience Database</h1>
+    <br/>
+
+    <p>A database of eldritch artifacts and encounters. The cultist's #1 resource!</p>
+    <br/>
+
+    <h3>WELCOME, SCHOLAR.</h3>
+
+    </main>
+  );
+}
